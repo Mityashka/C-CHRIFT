cec888480bacaf77083d95b9991d0679b3588114
Mityashka <dima.kuzkin@yanex.ru>
2024-02-13 16:24:03 +0300

    Добавьте файлы проекта.

---
+++ C#CHRIFT/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string inputPath = "input.txt";
+        string outputPath = "output.txt";
+
+        try
+        {
+            string text = File.ReadAllText(inputPath);
+
+            string[] words = text.Split(new char[] { ' ', '\n', '\r', '.', ',', ':', ';', '!', '?', '-', '"', '\'' },
+                                                     StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                string cleanedWord = word.ToLower();
+                if (wordCounts.ContainsKey(cleanedWord))
+                {
+                    wordCounts[cleanedWord]++;
+                }
+                else
+                {
+                    wordCounts[cleanedWord] = 1;
+                }
+            }
+
+            var sortedWordCounts = wordCounts.OrderByDescending(pair => pair.Value);
+
+            using (StreamWriter writer = new StreamWriter(outputPath))
+            {
+                foreach (var pair in sortedWordCounts)
+                {
+                    writer.WriteLine($"{pair.Key}: {pair.Value}");
+                }
+            }
+
+            Console.WriteLine("Идет подсчет слов..");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("ошибка");
+        }
+    }
+}
